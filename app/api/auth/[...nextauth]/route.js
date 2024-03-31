a54270c832d444b6b1a6b08a8ef947ca3990c3ba
Anita <jelicanita7@gmail.com>
2024-03-31 23:31:08 +0200

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -35,12 +35,12 @@ const handler = NextAuth({
             try {
                 await connectToDB();
     
-                // check if a user already exists
+                // check if user already exists
                 const userExists = await User.findOne({
                     email: profile.email
                 });
     
-                // if not, create a new user
+                // if not, create a new document and save user in MongoDB
                 if(!userExists) {
                     await User.create({
                         email: profile.email,
@@ -51,7 +51,8 @@ const handler = NextAuth({
     
                 return true;
             } catch (error) {
-                
+                console.log("Error checking if user exists: ", error.message);
+                return false
             }
         }
     } 
